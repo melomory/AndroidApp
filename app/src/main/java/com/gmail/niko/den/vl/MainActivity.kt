62271f3aa6f272dc352c8ad780cc5683d58a88b9
melomory <niko.den.vl@gmail.com>
2021-10-06 22:12:40 +0400

    Created a new project with an empty activity

---
+++ app/src/main/java/com/gmail/niko/den/vl/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.niko.den.vl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
